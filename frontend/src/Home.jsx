d0d74b4b2751c77c00d5a387bf90835f89020890
Vishesh Kumar <154086150+ItsRacoon@users.noreply.github.com>
2024-12-03 02:07:26 +0530

    Add files via upload

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,6 +1,7 @@
 import react from "react";
 import Navbar from "./Navbar"; 
 import Cards from "./Cards";
+import { Link } from "react-router-dom";
 import './home.css';
 const Home = () =>{
     return (
@@ -13,11 +14,14 @@ const Home = () =>{
   <span>
     Classify
   </span>
-  {/* <br />
-  <span>
-    PARALLAX PIXEL STARS
-  </span> */}
-</div>
+  <br />
+  <p className="homeText">Classify is a web platform that streamlines student-teacher interactions. Teachers can create classes, share assignments, and manage submissions, while students can join classes, access materials, and submit their work. It simplifies classroom management and enhances collaboration.
+  </p>
+
+
+  </div>
+  
+  
      </div>
      
      
