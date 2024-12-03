43a9d347d64be9c331c4db1baaa167204004c6ff
Vishesh Kumar <154086150+ItsRacoon@users.noreply.github.com>
2024-12-04 01:33:47 +0530

    version2.2

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from './Signup'
+import Login from './Login'
+import Home from './Home'
+import Cards from './Cards'
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+   
+    <BrowserRouter>
+      <Routes>
+        <Route path= "/" element ={ <Home />} /> 
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path ="/Cards" element={<Cards />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
